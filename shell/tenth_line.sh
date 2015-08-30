b6ae200f4a693cbeab4735559264e23f93f1ed8f
dsdshcym <dsdshcym@gmail.com>
2015-08-30 19:54:07 +0800

    Solved Tenth Line

---
+++ shell/tenth_line.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# How would you print just the 10th line of a file?
+
+sed -n '10p' file.txt
+
+# There's at least three different solutions. Try to explore all possibilities.
