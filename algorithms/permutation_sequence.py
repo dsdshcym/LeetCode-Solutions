564b33ae4e4e8df9683a90fb2d9b5f3c484da39a
Yiming Chen <dsdshcym@gmail.com>
2015-10-15 18:50:52 +0800

    Solved Permutation Sequence

--- algorithms/permutation_sequence.py
+++ algorithms/permutation_sequence.py
@@ -1,14 +1,24 @@
-from itertools import permutations
+from math import factorial
 
 class Solution(object):
+    def getStrPermutation(self, n_str, k):
+        n = len(n_str)
+        if n == 1:
+            return n_str[0]
+        r = (k - 1) / factorial(n - 1)
+        new_k = k - r * factorial(n - 1)
+        digit = n_str[r]
+        n_str.remove(digit)
+        return digit + self.getStrPermutation(n_str, new_k)
+
     def getPermutation(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: str
         """
-        NUMS = '123456789'
-        return ''.join(list(permutations(NUMS[:n]))[k - 1])
+        n_str = [str(i) for i in range(1, n + 1)]
+        return self.getStrPermutation(n_str, k)
 
 t = Solution()
 
