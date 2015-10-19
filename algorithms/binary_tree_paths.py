e54140be88bee5ceb26a8ad9ca29a07ef877ff94
Yiming Chen <dsdshcym@gmail.com>
2015-10-19 16:52:17 +0800

    Solved Binary Tree Paths

--- algorithms/binary_tree_paths.py
+++ algorithms/binary_tree_paths.py
@@ -8,22 +8,21 @@
 class Solution(object):
     def dfs(self, root, path):
         if root.left is None and root.right is None:
-            self.ans.append(path)
+            self.ans.append(path + str(root.val))
             return
         if root.left:
-            self.dfs(root.left, path + '->' + str(root.left.val))
+            self.dfs(root.left, path + str(root.val) + '->')
         if root.right:
-            self.dfs(root.right, path + '->' + str(root.right.val))
+            self.dfs(root.right, path + str(root.val) + '->')
 
     def binaryTreePaths(self, root):
         """
         :type root: TreeNode
         :rtype: List[str]
         """
-        if root is None:
-            return []
         self.ans = []
-        self.dfs(root, str(root.val))
+        if root:
+            self.dfs(root, '')
         return self.ans
 
 def test_Solution():
