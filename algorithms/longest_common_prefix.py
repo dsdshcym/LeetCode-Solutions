df3e4eac6283c15754171eb34ca04b30087e928d
Yiming Chen <dsdshcym@gmail.com>
2015-10-26 14:45:13 +0800

    Add reduce solution for Longest Common Prefix

--- algorithms/longest_common_prefix.py
+++ algorithms/longest_common_prefix.py
@@ -1,4 +1,10 @@
 class Solution(object):
+    def prefix(self, a, b):
+        for i in xrange(min(len(a), len(b))):
+            if b.find(a[:i + 1]) != 0:
+                return a[:i]
+        return a if len(a) < len(b) else b
+
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
@@ -6,17 +12,12 @@ class Solution(object):
         """
         if strs == []:
             return ''
-        ans = ''
-        for i in xrange(len(strs[0])):
-            tmp = strs[0][:i + 1]
-            for s in strs:
-                if s[:i + 1] != tmp:
-                    return strs[0][:i]
-        return strs[0]
+        return reduce(self.prefix, strs)
 
 t = Solution()
 
 assert t.longestCommonPrefix([]) == ''
+assert t.longestCommonPrefix(['']) == ''
 assert t.longestCommonPrefix(['', '']) == ''
 assert t.longestCommonPrefix(['a', 'b']) == ''
 assert t.longestCommonPrefix(['a', 'ab']) == 'a'
