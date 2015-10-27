d71f99e189a7237ae642ad0c54afc6a51ad0e0fa
Yiming Chen <dsdshcym@gmail.com>
2015-10-27 12:31:53 +0800

    Add recursive solution for Add Digits

--- algorithms/add_digits.py
+++ algorithms/add_digits.py
@@ -1,9 +1,13 @@
+from operator import add
+
 class Solution(object):
     def addDigits(self, num):
         """
         :type num: int
         :rtype: int
         """
+        n = reduce(add, map(int, str(num)))
+        return n if n <= 9 else self.addDigits(n)
 
         # https://en.wikipedia.org/wiki/Digital_root
         return 1 + (num - 1) % 9 if num > 0 else 0
