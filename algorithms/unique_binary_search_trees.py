97dbb6d1f9e11d6d8015c4029894d9f0671edf25
Yiming Chen <dsdshcym@gmail.com>
2015-10-29 18:16:09 +0800

    Add mathematical solution for Unique BSTs

--- algorithms/unique_binary_search_trees.py
+++ algorithms/unique_binary_search_trees.py
@@ -4,13 +4,20 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        f = [1, 1]
-        for i in xrange(2, n + 1):
-            f.append(sum([f[i - 1 - j] * f[j] for j in xrange(i)]))
-        return f[n]
+        if n == 1:
+            return 1
+        return self.numTrees(n - 1) * (4 * n - 2) / (n + 1)
+
+        # f = [1, 1]
+        # for i in xrange(2, n + 1):
+        #     f.append(sum([f[i - 1 - j] * f[j] for j in xrange(i)]))
+        # return f[n]
 
 t = Solution()
 
 assert t.numTrees(1) == 1
 assert t.numTrees(2) == 2
 assert t.numTrees(3) == 5
+
+for i in xrange(10):
+    print t.numTrees(i)
