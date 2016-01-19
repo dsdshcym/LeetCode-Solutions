71c4172ac791e987585b60ec6eb28890d0e825f4
dsdshcym <dsdshcym@gmail.com>
2016-01-19 11:07:31 +0800

    Improve the solution to Intersection Of Two Linked Lists
    According to
    [[https://leetcode.com/discuss/66203/java-solution-without-knowing-the-difference-in-len][Java
    solution without knowing the difference in len! - Leetcode Discuss]]

--- algorithms/intersection_of_two_linked_lists.py
+++ algorithms/intersection_of_two_linked_lists.py
@@ -10,27 +10,17 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        lenA = lenB = 0
         pA = headA
         pB = headB
-        while pA:
-            lenA += 1
-            pA = pA.next
-        while pB:
-            lenB += 1
-            pB = pB.next
-
-        pA = headA
-        pB = headB
-        while lenA > lenB:
-            pA = pA.next
-            lenA -= 1
-        while lenB > lenA:
-            pB = pB.next
-            lenB -= 1
 
         while pA != pB:
-            pA = pA.next
-            pB = pB.next
+            if not pA:
+                pA = headB
+            else:
+                pA = pA.next
+            if not pB:
+                pB = headA
+            else:
+                pB = pB.next
 
         return pA
