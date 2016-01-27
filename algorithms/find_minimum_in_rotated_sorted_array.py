812e72fac8668b4c7ea9d620e50bdc3641f0260b
dsdshcym <dsdshcym@gmail.com>
2016-01-27 15:03:02 +0800

    Solved Find Minimum in Rotated Sorted Array

---
+++ algorithms/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                return nums[i + 1]
+        return nums[0]
+
+t = Solution()
+assert(t.findMin([1, 2]) == 1)
+assert(t.findMin([4, 5, 6, 7, 0, 1, 2]) == 0)
+print("tests passed")
