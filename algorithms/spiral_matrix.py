00e6e616bbce1f6eea2921514cefe5c22fcda507
dsdshcym <dsdshcym@gmail.com>
2016-01-30 15:36:48 +0800

    Add another solution for Spiral Matrix

--- algorithms/spiral_matrix.py
+++ algorithms/spiral_matrix.py
@@ -6,28 +6,30 @@ class Solution(object):
         """
         if not matrix:
             return []
-        MOVE = [[0, +1], [+1, 0], [0, -1], [-1, 0]]
-        M, N = len(matrix), len(matrix[0])
-        visited = [[False,] * N for i in range(M)]
+        m, n = len(matrix), len(matrix[0])
         ans = []
 
-        def dfs(i, j, d):
-            if i < 0 or j < 0 or i >= M or j >=N or visited[i][j]:
-                return False
-            ans.append(matrix[i][j])
-            visited[i][j] = True
-            new_i, new_j = i + MOVE[d][0], j + MOVE[d][1]
-            if not dfs(new_i, new_j, d):
-                d = (d + 1) % 4
-                new_i, new_j = i + MOVE[d][0], j + MOVE[d][1]
-                if not dfs(new_i, new_j, d):
-                    return False
-            return True
+        x, y = 0, 0
+        while m > 0 and n > 0:
+            if m == 1:
+                ans += matrix[x][y:y+n]
+                break
+            if n == 1:
+                ans += [matrix[x+i][y+n-1] for i in range(m)]
+                break
+            ans += matrix[x][y:y+n-1] + \
+                   [matrix[x+i][y+n-1] for i in range(m-1)] + \
+                   matrix[x+m-1][y+n-1:y:-1] + \
+                   [matrix[x+i][y] for i in range(m-1, 0, -1)]
+            x += 1
+            y += 1
+            m -= 2
+            n -= 2
 
-        dfs(0, 0, 0)
         return ans
 
 t = Solution()
 assert(t.spiralOrder([[1]]) == [1])
 assert(t.spiralOrder([[1, 2], [3, 4]]) == [1, 2, 4, 3])
+assert(t.spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [1, 2, 3, 6, 9, 8, 7, 4, 5])
 print("tests passed")
