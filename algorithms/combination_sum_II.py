fe2985e84c481da295230860393862b54fdd59d4
dsdshcym <dsdshcym@gmail.com>
2016-01-30 14:45:34 +0800

    A faster solution for Combination Sum II

--- algorithms/combination_sum_II.py
+++ algorithms/combination_sum_II.py
@@ -23,6 +23,31 @@ class Solution(object):
         dfs(0, 0, [])
         return ans
 
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if not candidates:
+            return []
+        N = len(candidates)
+        ans = []
+        candidates.sort()
+        for i in range(N):
+            if i > 0 and candidates[i] == candidates[i-1]:
+                continue
+            if candidates[i] == target:
+                ans.append([candidates[i]])
+            elif candidates[i] > target:
+                break
+            else:
+                ans.extend([[candidates[i]] + l
+                            for l in self.combinationSum2(candidates[i+1:], target-candidates[i])
+                            if l])
+        return ans
+
 t = Solution()
 print(t.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
 print(t.combinationSum2([1], 1))
