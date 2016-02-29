add70f50a229805ae83a4666993cfcaf27072fe5
dsdshcym <dsdshcym@gmail.com>
2016-02-29 20:26:02 +0800

    Update Palindrome Linked List
    1. Use fast and slow pointer to find middle
    2. Reverse second half
    3. Compare first half and reversed second half

--- algorithms/palindrome_linked_list.py
+++ algorithms/palindrome_linked_list.py
@@ -1,5 +1,3 @@
-#+TAG: NEEDS_IMPROVE
-
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -7,18 +5,32 @@
 #         self.next = None
 
 class Solution(object):
+    def reverse(self, start, end):
+        if not start or not start.next:
+            return
+        prev, p = start, start.next
+        while p != end:
+            p.next, prev, p = prev, p, p.next
+        start.next.next, start.next = end, prev
+
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        pointer = head
-        array = []
-        while (pointer != None):
-            array.append(pointer.val)
-            pointer = pointer.next
-        N = len(array)
-        for i in range(N / 2):
-            if (array[i] != array[N - i - 1]):
+        if not head:
+            return True
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+
+        self.reverse(slow, None)
+
+        l, r = head, slow.next
+        while l and r:
+            if l.val != r.val:
                 return False
+            l, r = l.next, r.next
+
         return True
