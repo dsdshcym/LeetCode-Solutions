fdffc46d5cf37d7f57a8c628cd3e70a6b32932d4
dsdshcym <dsdshcym@gmail.com>
2016-03-03 10:06:03 +0800

    Update Add Binary
    Implement add binary without using bin()

--- algorithms/add_binary.py
+++ algorithms/add_binary.py
@@ -5,7 +5,30 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        return bin(int(a, 2) + int(b, 2))[2:]
+        ans = []
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        while i >= 0 or j >= 0:
+            if i < 0:
+                num_a = 0
+            else:
+                num_a = ord(a[i]) - ord('0')
+                i -= 1
+
+            if j < 0:
+                num_b = 0
+            else:
+                num_b = ord(b[j]) - ord('0')
+                j -= 1
+
+            s = num_a + num_b + carry
+            carry = s // 2
+            ans.append(str(s % 2))
+
+        if carry:
+            ans.append(str(carry))
+
+        return ''.join(ans[::-1])
 
 t = Solution()
 assert(t.addBinary('100', '1') == '101')
