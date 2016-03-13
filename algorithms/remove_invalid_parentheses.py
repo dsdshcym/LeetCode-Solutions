22f40094a1908401e6adc7679e532d0e0eecfdd4
dsdshcym <dsdshcym@gmail.com>
2016-03-13 11:34:45 +0800

    Solved Remove Invalid Parentheses BFS

--- algorithms/remove_invalid_parentheses.py
+++ algorithms/remove_invalid_parentheses.py
@@ -1,35 +1,36 @@
+from collections import deque
+
 class Solution(object):
+    def calc(self, s):
+        a, b = 0, 0
+        V = {'(': +1, ')': -1}
+        for c in s:
+            a += V.get(c, 0)
+            b += a < 0
+            a = max(0, a)
+        return a + b
+
     def removeInvalidParentheses(self, s):
         """
         :type s: str
         :rtype: List[str]
         """
-        ans = []
-        stack = []
-        self.removeInvalidParentheses_helper(s, 0, "", stack, ans)
-        return list(set(ans))
-
-    def removeInvalidParentheses_helper(self, s, index, current, stack, ans):
-        if index >= len(s):
-            if not stack:
-                if ans and len(current) == len(ans[-1]):
-                    ans.append(current)
-                elif (not ans) or (len(current) > len(ans[-1])):
-                    ans[:] = [current]
-            return
-        if s[index] == '(':
-            stack.append('(')
-            self.removeInvalidParentheses_helper(s, index + 1, current + '(', stack, ans)
-            stack.pop(-1)
-            self.removeInvalidParentheses_helper(s, index + 1, current, stack, ans)
-        elif s[index] == ')':
-            if stack and stack[-1] == '(':
-                stack.pop(-1)
-                self.removeInvalidParentheses_helper(s, index + 1, current + ')', stack, ans)
-                stack.append('(')
-            self.removeInvalidParentheses_helper(s, index + 1, current, stack, ans)
-        else:
-            self.removeInvalidParentheses_helper(s, index + 1, current + s[index], stack, ans)
+        visited = set([s])
+        queue = deque([s])
+        ans = set()
+        while queue:
+            current = queue.popleft()
+            miss_parentheses = self.calc(current)
+            if miss_parentheses == 0:
+                ans.add(current)
+                continue
+            for i in range(len(current)):
+                next_string = current[:i] + current[i+1:]
+                if next_string not in visited and self.calc(next_string) < miss_parentheses:
+                    visited.add(next_string)
+                    queue.append(next_string)
+        return list(ans)
 
 t = Solution()
 print(t.removeInvalidParentheses("()())()"))
+print(t.removeInvalidParentheses(")(((((((("))
